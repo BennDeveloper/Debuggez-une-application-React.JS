8f2db9f1268282bd27dee3de9ddbc3d923612dda
bennabbou <bennabbou.a@gmail.com>
2024-08-11 19:33:15 +0200

    Correction dr l'ordre des evenements

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,25 +7,27 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  
+   // Correction dr l'ordre des evenements
+  const byDateAsc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
   // Fonction pour mettre à jour l'image
   const nextCard = () => {
-    if (byDateDesc && byDateDesc.length > 0) {
-      setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
+    if (byDateAsc && byDateAsc.length > 0) {
+      setIndex((prevIndex) => (prevIndex < byDateAsc.length - 1 ? prevIndex + 1 : 0));
     }
   };
 
   useEffect(() => {
     const timer = setTimeout(nextCard, 5000); // Utiliser setTimeout pour définir la durée
     return () => clearTimeout(timer); // Annuler le minuteur lors du démontage du composant ou lors du changement de index
-  }, [index, byDateDesc]); // Ajouter byDateDesc comme dépendance
+  }, [index, byDateAsc]); // Ajouter byDateAsc comme dépendance
 
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
+      {byDateAsc?.map((event, idx) => (
         <div key={event.id}>
           <div
             className={`SlideCard SlideCard--${
@@ -43,9 +45,9 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateAsc.map((_, radioIdx) => (
                 <input
-                  key={byDateDesc[radioIdx].id}
+                  key={byDateAsc[radioIdx].id}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
